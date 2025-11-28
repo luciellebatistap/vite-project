991d285d4ad00f3c87233bcf4349f4100d01cd1f
luciellebatistap <luciellebp@gmail.com>
2025-11-28 19:50:54 -0300

    ooo

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,24 @@ import { useState } from "react"
 import './App.css'
 function App() {
   const [tarefa, setTarefa] = useState("")
-  const [lista, setLista] = useState([]
-  return (
-    
+  const [lista, setLista] = useState([])
+  function adicionarTarefa() {
+    const texto = tarefa.trim();       
+    if (texto === "") return;
+    setLista([...lista, texto]);
+    setTarefa("");
+  }
+    return (
     <>
     <div className="container"> 
     <h1> Lista de Tarefas</h1>
     <input type="text" placeholder="Digite uma tarefa" value={tarefa} onChange={(e) => setTarefa(e.target.value)}/>
-   <button>Adicionar</button>
+   <button onClick={adicionarTarefa}> Adicionar</button>
+   <ul>
+  {lista.map((item, index) => (
+    <li key={index}>{item}</li>
+  ))}
+</ul>
     </div>
      </>
   
